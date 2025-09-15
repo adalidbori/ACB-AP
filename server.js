8653cb4b4303a9b2a6c5aceb3651dc5350dd7852
Adalid Bori <adalid.bori@gmail.com>
2025-09-15 12:05:44 -0400

    port change

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ const { BlobServiceClient, BlobClient, StorageSharedKeyCredential, BlobSASPermis
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configuración de CORS y parsers
 app.use(cors());
